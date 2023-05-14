33a90b0ce48a33067bab9a14edf9cc2b786620d8
IvoMbrandao <113533224+IvoMBrandao@users.noreply.github.com>
2023-05-14 20:03:18 -0300

    Separando components

--- src/pages/inicio/index.jsx
+++ src/pages/inicio/index.jsx
@@ -1,58 +1,16 @@
-import React from "react"; 
-import{
-    View,
-    StyleSheet,
-    Text,
-    StatusBar,
-    TouchableOpacity
-} from 'react-native';
+import React from "react";
+import{View} from 'react-native';
+import Header from "../../components/cabecalho";
+export default function Inicio(){
+return(
 
-import{Feather} from '@expo/vector-icons';
-const StatusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22: 64 ;
 
-export default function Header({name}){
-    return(
-        <View style={styles.container}>
-            <View style={styles.content}>
-            <Text style={styles.username}>Ivo Felipe</Text> 
-            <TouchableOpacity activeOpacity={0.6} style={styles.buttomUser}>
-         <Feather name="user" size={35} color='#FFF'/>
-         </TouchableOpacity>
-            </View>
-        </View>
-    )
+<View>
+  <Header/>
+  
+</View>
+
+);
+
+
 }
-
-const styles = StyleSheet.create({
-    container:{
-        backgroundColor:'#008080',
-        paddingTop:StatusBarHeight,
-        flexDirection:"row",
-        paddingStart:16,
-        paddingEnd:16,
-        paddingBottom:25,
-        borderBottomLeftRadius:5,
-        borderBottomRightRadius:5,
-
-    },
-    content:{
-            flex:1,
-            alignItems:"center",
-            flexDirection:'row',
-            justifyContent:'space-between',
-    },
-    username:{
-        fontSize:18,
-        color:'#FFF',
-        fontWeight:'bold'        
-
-    },
-    buttomUser:{
-        width:60,
-        height:60,
-        backgroundColor:'rgba(255, 255, 255, 0.5)',
-        justifyContent:'center', 
-        alignItems:'center',
-        borderRadius: 60 /2,   
-    }
-    })
